93b9c1681cd0fa39337ce5a1c3feea1f42d58dd6
obispowned <alguma90@gmail.com>
2021-03-19 20:11:26 +0100

    hacer pruebas

--- mi_micropaint/main.c
+++ mi_micropaint/main.c
@@ -11,11 +11,12 @@ typedef struct s_background
 
 typedef struct s_form
 {
-    char        type_f;
+    char        type;
     float       x;
     float       y;
     float       width;
     float       height;
+    char        pencil;
 }               t_form;
 
 
@@ -85,10 +86,52 @@ int     paint_piece(char **screen)
     return(1);
 }
 
+void            double_kill(char **str)
+{
+    int         i;
+
+    i = 0;
+    while (str[i])
+    {
+        free(str[i]);
+        i++;
+    }
+    free(str);
+}
+
+void            focus_perimetro()
+{
+
+}
+
+void            focus_area()
+{
+
+}
+
+void            save_forms(FILE *file, char **screen, t_form *forma)
+{
+    int         ret;
+
+    if ((ret = fscanf(file, "%c %f %f %f %f %%c", &forma->type, &forma->x, &forma->y, &forma->width, &forma->height, &forma->pencil) != 6))
+    {
+        printerror("Un error al leer del archivo");
+    }
+    if (forma->type == 'r')
+        focus_perimetro();
+    else if (forma->type == 'R')
+        focus_area();
+    else
+    {
+        printerror("Un error ya que el primer caractwr no es ni r ni R");
+    }
+}
+
 int             main(int argc, char **argv)
 {
     FILE        *file;
     t_backgound bg;
+    t_form      forma;
     char        **screen;
     int         i;
 
@@ -100,13 +143,15 @@ int             main(int argc, char **argv)
         printerror("Error: File Corrupted\n");
     printf("aqui hay un with de %d, y height de %d\n", bg.width, bg.height);
     screen = (char **)malloc(sizeof(char *) * bg.height + 1);
-    while( i < bg.width)
+    while(i < bg.width)
     {
         screen[i] = (char *)malloc(sizeof(char) * bg.width + 1);
        i++;
     }
     save_bg(screen , &bg);
+    save_forms(file, screen, &forma);
     /*AQUI FALTA DE METER UN BUCLE CON SCANF PARA CUALQUIER NUM DE FIGURAS*/
     paint_piece(screen);
+    double_kill(screen);
     return (0);
 }
